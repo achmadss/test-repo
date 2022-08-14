8f26fcc4e9c32aaa8299716a7c3f26bb85adddf9
achmadss <anakinskywalk1@gmail.com>
2022-08-14 15:35:42 +0700

    update

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(Routes)
 
-connectDb().then(() => {
-    db.sync()
-    .then(() => {
-        console.log("Synced db.");
-    })
-    .catch((error) => {
-        console.error("Failed to sync db: ", error);
-    })
+Routes.get('/', (req, res) => {
+    res.send('Hello World')
 })
 
-async function connectDb() {
-    try {
-        await db.authenticate()
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+// app.use(Routes)
+
+// connectDb().then(() => {
+//     db.sync()
+//     .then(() => {
+//         console.log("Synced db.");
+//     })
+//     .catch((error) => {
+//         console.error("Failed to sync db: ", error);
+//     })
+// })
+
+// async function connectDb() {
+//     try {
+//         await db.authenticate()
+//         console.log('Connection has been established successfully.');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+// }
 
 app.listen(port, () => {
   console.log(`Test app listening on port ${port}`)
