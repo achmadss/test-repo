a11e1476e819ecc26adf0c5e5f7d383a7b4f1f06
achmadss <anakinskywalk1@gmail.com>
2022-08-14 15:35:52 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 Routes.get('/', (req, res) => {
     res.send('Hello World')
