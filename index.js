8c285bcaa0f8434aa173d3964eb24447af2f0c32
achmadss <anakinskywalk1@gmail.com>
2022-08-14 14:46:09 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const Routes = require('./routes/routes')
 const db = require("./config/database")
 
 const app = express()
 const port = 3000
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(Routes)
