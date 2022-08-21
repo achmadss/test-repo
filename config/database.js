f985e0a8b4faf2be7bb0499c080f82ac9c1ebd09
achmadss <anakinskywalk1@gmail.com>
2022-08-21 13:34:14 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,14 +1,44 @@
 import Sequelize from "sequelize"
+import mysql from "mysql2"
 
-const db = new Sequelize(
-    'test_db',
-    'root',
-    'password', 
-    {
-        host:'test-mysql',
-        dialect:'mysql',
-        port:3306,
+let database = 'test_db'
+let username = 'root'
+let password = 'password'
+
+let host = 'test-mysql'
+let dialect = 'mysql'
+let port = 3306
+
+const initDB = async () => {
+    // create db if not exist
+    const connection = mysql.createConnection({ host, port, user, password })
+    connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`)
+    // connect to db
+    const db = new Sequelize(
+        database,
+        username,
+        password, 
+        {
+            host:host,
+            dialect:dialect,
+            port:port,
+        }
+    )
+    // authenticate and sync
+    try {
+        await db.authenticate().then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        
+        await db.sync().then(() => {
+            console.log("Synced db.");
+        })
+
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-)
+}
 
-export { db }
+
+
+export { initDB }
--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 import express from "express"
 import { Routes } from "./routes/routes.js"
-import { db } from "./config/database.js"
+import { initDB } from "./config/database.js"
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use(Routes)
 
-connectDb().then(() => {
-    db.sync()
-    .then(() => {
-        console.log("Synced db.");
-    })
-    .catch((error) => {
-        console.error("Failed to sync db: ", error);
-    })
-})
-
-async function connectDb() {
-    try {
-        await db.authenticate()
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+initDB()
 
 app.listen(port, () => {
   console.log(`Test app listening on port ${port}`)
