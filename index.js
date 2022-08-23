2003ce29b1eab0e9fef1af74c114dbfd2d2d5d64
achmadss <anakinskywalk1@gmail.com>
2022-08-23 13:16:36 +0700

    back to v0.0.6

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express"
 import { Routes } from "./routes/routes.js"
-import { initDB } from "./config/database.js"
+// import { initDB } from "./config/database.js"
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(Routes)
+// app.use(Routes)
 
-initDB()
+Routes.get('/', (req, res) => {
+  res.send('Hello World!!')
+})
+
+// initDB()
 
 app.listen(port, () => {
   console.log(`Test app listening on port ${port}`)
